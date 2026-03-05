2cb704d425b008904b4f9c9ece1b8176b3cc7093
srinivas <srinivas23peddada@gmail.com>
2026-03-05 12:10:26 +0530

    api.js code updated

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
-  baseURL: "https://omnitaas-task.onrender.com",
+  baseURL: "https://omniitaas-task.onrender.com"
 });
 
 export default API;
